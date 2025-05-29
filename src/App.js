2b68423810c7eac2dfa2b0d302e4c7bd5057993e
gregorikouk <gregorikouk@gmail.com>
2025-05-29 14:18:01 +0300

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, Link, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate, Link, useNavigate } from "react-router-dom";
 import {
   collection,
   doc,
